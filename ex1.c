f625699e3836933e95d7169961aacefc5e5104bd
anneschoonmaker <atschoonmaker@gwu.edu>
2021-10-07 17:26:27 -0400

    ex1

--- ex1.c
+++ ex1.c
@@ -12,21 +12,55 @@
 
 /* shared variable for the threads to update */
 volatile int var = WAITING_FOR_SAMPLE;
-
+pthread_mutex_t lock;
+static void * pthread_fn(void *arg){
+	//need a lock
+	while(1){
+	printf("%s", "in thread \n");
+	while( var != WAITING_FOR_SAMPLE);
+	pthread_mutex_lock(&lock);
+	if(var == WAITING_FOR_SAMPLE){
+			var = circle();
+	pthread_mutex_unlock(&lock);
+	}
+	// var = circle();
+	// pthread_mutex_unlock(&lock);
+	}
+	//need to unlocl 
+	return NULL;
+}
 int main(void)
 {
+	pthread_t thread1;
 	uint32_t total_in_arc = 0;
-
+	//pthread create makes a thread and has a function associated with it 
+	//for loop that creates processes ?
+	for(int i = 0 ; i < WORKER_COUNT; i++){
+		// pthread_create();
+		pthread_create(&thread1, NULL, &pthread_fn, NULL);
+	}
+	
 	for (uint32_t i = 0; i < NITERS; i++)
 	{
+		//just need to spin 
+		while(var == WAITING_FOR_SAMPLE){
+		}
+		pthread_mutex_lock(&lock);
+		//critical section
+		//must be ready and have a sample 
+		//while(waiting for sample)
 		if (circle() == INSIDE_CIRCLE)
 		{
 			total_in_arc++;
 		}
+		pthread_mutex_unlock(&lock);
 	}
+	// if(pthread_join(thread1))
 
 	/* We multiply by four because circle() uses an 1/4 circular arc */
 	printf("The value of pi is: %f\n", (double)total_in_arc / NITERS * 4);
 
 	return 0;
+
 }
+
